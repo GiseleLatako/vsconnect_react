0568e1e97c6035f39dcaf9fbd2cbfbe4251fd7d3
Gisele <giselesenai2022@gmail.com.br>
2023-08-09 21:56:47 -0300

    componentes header e footer no react

--- vsconnect/src/main.tsx
+++ vsconnect/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 //componentes
 import Home from "./pages/Home/"
+import Footer from './components/Footer'
 import ListaServicos from "./pages/ListaServicos/"
+import header from "./components/header"
 
 //estilização global
 import "./index.css"
@@ -11,7 +13,10 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    
     {/* <Home /> */}
-    <ListaServicos />
+    <Home/>
+    {/* <ListaServicos /> */}
+    <Footer/>
   </React.StrictMode>,
 )
